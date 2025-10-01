adb1f3dbdbda399efc2a108b85682ac58109d6d8
chimchilambe123 <chimwemwechilambe@gmail.com>
2025-10-01 14:46:24 +0200

    Registration screen done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from './screen/RegisterScreen';
 import LoginScreen from './screen/LoginScreen';
+import HomeScreen from './screen/HomeScreen'; 
 
 export type RootStackParamList = {
   Login: undefined;
+  Register: undefined;
   Home: undefined;
 };
 
@@ -13,12 +16,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+     
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={HomeScreen} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screen/HomeScreen.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../App';
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+
+const HomeScreen: React.FC = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the App!</Text>
+      <Text style={styles.subtitle}>
+        This is your home screen after login.
+      </Text>
+
+
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#555',
+    textAlign: 'center',
+    marginBottom: 30
+  },
+  button: {
+    backgroundColor: '#6366F1',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    width: '50%'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
